62e05678f0fa49c362eeee5521b35502e3ace2d9
oluwatamilore Ajayi <ajayitamilore@gmail.com>
2023-01-24 14:08:36 +0100

    Added new1

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3001", "https://tammyfooddelivery.onrender.com"],
+  })
+);
 const foodRoute = require("./routes/foodRoute");
 const userRoute = require("./routes/userRoute");
 const orderRoute = require("./routes/orderRoute");
